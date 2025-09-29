86ebe7c1ae7b70dc8de6fc6536d4a6b2eb60eeb5
Maya Uwaydat <157422886+mayooueidat-uva@users.noreply.github.com>
2025-09-29 19:37:28 -0400

    clean.py DRAFT have yet to include logging

--- clean.py
+++ clean.py
@@ -3,53 +3,76 @@ import duckdb
 import logging
 
 def clean_parquet():
-    con = None # what does this mean. 
+    con = None
     try:
         # connect to local database
         con = duckdb.connect(database="TEST.db", read_only=False)
-        # removing trips that are more than one day in length YELLOW
+        # removing trips that are more than one day in length YELLOW CABS
         con.execute(f"""
-            ALTER TABLE yellow_trip_data4
+            ALTER TABLE yellow_tripdata_06
             ADD COLUMN trip_duration DOUBLE;
 
-           UPDATE yellow_trip_data4
+           UPDATE yellow_tripdata_06
            SET trip_duration = EXTRACT(EPOCH FROM (tpep_dropoff_datetime - tpep_pickup_datetime));
 
-           DELETE FROM yellow_trip_data4
+           DELETE FROM yellow_tripdata_06
            WHERE trip_duration > 86400;
         """)
-        # removing trips that are more than one day in length GREEN
+        # removing trips that are more than one day in length GREEN CABS
         con.execute(f"""    
-            ALTER TABLE green_tripdata_01 
+            ALTER TABLE green_tripdata_06
             ADD COLUMN trip_duration DOUBLE;
 
-           UPDATE yellow_tripdata_01 
-           SET trip_duration = EXTRACT(EPOCH FROM (tpep_dropoff_datetime - tpep_pickup_datetime));
+           UPDATE green_tripdata_06 
+           SET trip_duration = EXTRACT(EPOCH FROM (lpep_dropoff_datetime - lpep_pickup_datetime));
 
-           DELETE FROM yellow_tripdata_01
+           DELETE FROM green_tripdata_06
            WHERE trip_duration > 86400;
         """)
-        # removing duplicate values (done - just needs test run) 
+        # removing duplicate values YELLOW CABS
         con.execute(f"""
             CREATE TABLE yellow_tripdata_clean AS 
-            SELECT DISTINCT * FROM yellow_tripdata_01;
+            SELECT DISTINCT * FROM yellow_tripdata_06;
 
-            DROP TABLE yellow_tripdata_01;
-            ALTER TABLE yellow_tripdata_clean RENAME TO yellow_tripdata_01;
+            DROP TABLE yellow_tripdata_06;
+            ALTER TABLE yellow_tripdata_clean RENAME TO yellow_tripdata_06;
         """)
-        # removing long trips (done - just needs test run) 
-        too_old = con.execute(f"""
-            DELETE FROM yellow_tripdata_01
+        # removing duplicate values GREEN CABS
+        con.execute(f"""
+            CREATE TABLE green_tripdata_clean AS 
+            SELECT DISTINCT * FROM green_tripdata_06;
+
+            DROP TABLE green_tripdata_06;
+            ALTER TABLE green_tripdata_clean RENAME TO green_tripdata_06;
+        """)
+        # removing long trips YELLOW CABS
+        con.execute(f"""
+            DELETE FROM yellow_tripdata_06
             WHERE trip_distance > 100;
         """)
-        # removing zero passengers (done - just needs test run) 
+        # removing long trips GREEN CABS
+        con.execute(f"""
+            DELETE FROM green_tripdata_06
+            WHERE trip_distance > 100;
+        """)
+        # removing zero passengers YELLOW CABS
         con.execute(f""" 
-            DELETE FROM yellow_tripdata_01
+            DELETE FROM yellow_tripdata_06
             WHERE passenger_count < 1;
         """)
-        # removing zero miles in length (done - just needs test run) 
+        # removing zero passengers GREEN CABS
+        con.execute(f""" 
+            DELETE FROM green_tripdata_06
+            WHERE passenger_count < 1;
+        """)
+        # removing zero miles in length YELLOW CABS
         con.execute(f"""
-            DELETE FROM yellow_tripdata_01 
+            DELETE FROM yellow_tripdata_06
+            WHERE trip_distance < 1;
+        """)
+        # removing zero miles in length GREEN CABS
+        con.execute(f"""
+            DELETE FROM green_tripdata_06
             WHERE trip_distance < 1;
         """)
     except Exception as e:
