91a71e584fcf86651256e730680537dcfe23b286
Maya Uwaydat <157422886+mayooueidat-uva@users.noreply.github.com>
2025-09-29 22:29:34 -0400

    biggest carbon emissions

--- analysis.py
+++ analysis.py
@@ -1,2 +1,16 @@
 import duckdb
 import logging
+
+# yellow
+  print(con.execute(f"""
+      SELECT *
+      FROM yellow_tripdata_06
+      WHERE trip_co2_kgs = (SELECT MAX(trip_co2_kgs) FROM yellow_tripdata_01);
+  """).fetchdf())
+
+# green
+  print(con.execute(f"""
+      SELECT *
+      FROM green_tripdata_06
+      WHERE trip_co2_kgs = (SELECT MAX(trip_co2_kgs) FROM green_tripdata_01);
+  """).fetchdf())
