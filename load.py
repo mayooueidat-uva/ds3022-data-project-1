51864b88394f1daffed9a1f74927b4491e90f490
Maya Uwaydat <157422886+mayooueidat-uva@users.noreply.github.com>
2025-09-29 20:45:44 -0400

    added logging to load.py

--- load.py
+++ load.py
@@ -2,18 +2,24 @@ import duckdb
 import os
 import logging
 
+# defining list of months/years to iterate through 
+months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+years = ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024"]
+
+# configuring the logging mechanism
 logging.basicConfig(
-    level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
-    filename='load.log'
+    filename='load.log',
+    filemode="a",
+    style="{", 
+    datefmt="%Y-%m-%d-%H:%M:%x", 
+    level="INFO"
 )
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     load_parquet()
 
-months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-years = ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024"]
-
+# defining function to load files 
 def load_parquet_files():
 
     con = None
@@ -23,37 +29,43 @@ def load_parquet_files():
         con = duckdb.connect(database='emissions.duckdb', read_only=False)
         logger.info("Connected to DuckDB instance")
 
+        # create empty YELLOW CAB table
         con.execute("""CREATE TABLE yellow_trip_data4(
-        tpep_pickup_datetime TIMESTAMP,
-        tpep_dropoff_datetime TIMESTAMP,
-        passenger_count INTEGER,
-        trip_distance DOUBLE
-        );
+            tpep_pickup_datetime TIMESTAMP,
+            tpep_dropoff_datetime TIMESTAMP,
+            passenger_count INTEGER,
+            trip_distance DOUBLE
+            );
         """)
-        con.execute("""CREATE TABLE green_trip_data4(
-        tpep_pickup_datetime TIMESTAMP,
-        tpep_dropoff_datetime TIMESTAMP,
-        passenger_count INTEGER,
-        trip_distance DOUBLE
-        );
-        """)
+        logger.info("created yellow cab table")
 
+        # create empty GREEN CAB table 
+        con.execute("""CREATE TABLE green_trip_data4(
+            lpep_pickup_datetime TIMESTAMP,
+            lpep_dropoff_datetime TIMESTAMP,
+            passenger_count INTEGER,
+            trip_distance DOUBLE
+            );
+        """)
+        logger.info("created green cab table")
+
+        # pulling parquet files to populate tables 
         for year in years:
             for month in months:
-
+                # populating YELLOW CAB table
                 con.execute(f"""
                     INSERT INTO yellow_trip_data
                     SELECT tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, trip_distance 
                     FROM read_parquet("https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month}.parquet");
                 """)
-
+                logger.info(f"added yellow cab data from year {year} month {month}") 
+                # populating GREEN CAB table
                 con.execute(f"""
                     INSERT INTO green_trip_data
                     SELECT lpep_pickup_datetime, lpep_dropoff_datetime, passenger_count, trip_distance 
                     FROM read_parquet("https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month}.parquet");
                 """)
-        
-        logger.info("Dropped table if exists")
+                logger.info(f"added green cab data from year {year} month {month}") 
 
     except Exception as e:
         print(f"An error occurred: {e}")
