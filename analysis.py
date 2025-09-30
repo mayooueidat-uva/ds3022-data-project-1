b93d065c95da8896b510b5ff5338a2984f84df27
Maya Uwaydat <157422886+mayooueidat-uva@users.noreply.github.com>
2025-09-29 23:26:17 -0400

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!

--- analysis.py
+++ analysis.py
@@ -1,22 +1,22 @@
 import duckdb
 import logging
 
-# yellow
-  print(con.execute(f"""
+# single largest carbon producing trip of the date range - YELLOW
+  print("carbon heaviest trip for yellow cabs\n",con.execute(f"""
       SELECT *
       FROM yellow_tripdata_06
       WHERE trip_co2_kgs = (SELECT MAX(trip_co2_kgs) FROM yellow_tripdata_01);
-  """).fetchdf())
+    """).fetchdf())
 
-# green
-  print(con.execute(f"""
+# single largest carbon producing trip of the date range - GREEN
+  print("carbon heaviest trip for green cabs\n",con.execute(f"""
       SELECT *
       FROM green_tripdata_06
       WHERE trip_co2_kgs = (SELECT MAX(trip_co2_kgs) FROM green_tripdata_01);
-  """).fetchdf())
+    """).fetchdf())
 
-# yellow hour of day 
-  print(con.execute(f"""
+# carbon heaviest hour of day - YELLOW 
+  print("carbon heaviest hour for yellow cabs\n",con.execute(f"""
     SELECT hour_of_day, 
     AVG(trip_co2_kgs) AS avg_co2
     FROM yellow_tripdata_07
@@ -25,8 +25,18 @@ import logging
     LIMIT 1;
   """).fetchdf())
 
-# green hour of day 
-  print(con.execute(f"""
+# carbon lightest hour of day - YELLOW 
+  print("carbon lightest hour for yellow cabs\n",con.execute(f"""
+    SELECT hour_of_day, 
+    AVG(trip_co2_kgs) AS avg_co2
+    FROM yellow_tripdata_07
+    GROUP BY hour_of_day
+    ORDER BY avg_co2 ASC
+    LIMIT 1;
+  """).fetchdf())
+
+# carbib heaviest hour of day - GREEN
+  print("carbon heaviest hour for green cabs\n",con.execute(f"""
     SELECT hour_of_day, 
     AVG(trip_co2_kgs) AS avg_co2
     FROM green_tripdata_07
@@ -35,8 +45,18 @@ import logging
     LIMIT 1;
   """).fetchdf())
 
-# yellow day of week 
-  print(con.execute(f"""
+# carbon lightest hour of day - GREEN
+  print("carbon lightest hour for green cabs\n",con.execute(f"""
+    SELECT hour_of_day, 
+    AVG(trip_co2_kgs) AS avg_co2
+    FROM green_tripdata_07
+    GROUP BY hour_of_day
+    ORDER BY avg_co2 ASC
+    LIMIT 1;
+  """).fetchdf())
+
+# carbon heaviest day of week - YELLOW
+  print("carbon heaviest day of week for yellow cabs\n",con.execute(f"""
     SELECT day_of_week, 
     AVG(trip_co2_kgs) AS avg_co2
     FROM green_tripdata_07
@@ -45,8 +65,18 @@ import logging
     LIMIT 1;
   """).fetchdf())
 
-# green day of week 
-  print(con.execute(f"""
+# carbon lightest day of week - YELLOW
+  print("carbon lightest day of week for yellow cabs\n",con.execute(f"""
+    SELECT day_of_week, 
+    AVG(trip_co2_kgs) AS avg_co2
+    FROM green_tripdata_07
+    GROUP BY day_of_week
+    ORDER BY avg_co2 ASC
+    LIMIT 1;
+  """).fetchdf())
+
+# carbon heaviest day of week - GREEN
+  print("carbon heaviest day of week for green cabs\n",con.execute(f"""
     SELECT day_of_week, 
     AVG(trip_co2_kgs) AS avg_co2
     FROM green_tripdata_07
@@ -55,8 +85,18 @@ import logging
     LIMIT 1;
   """).fetchdf())
 
-# yellow week of year 
-  print(con.execute(f"""
+# carbon lightest day of week - GREEN
+  print("carbon lightest day of week for green cabs\n",con.execute(f"""
+    SELECT day_of_week, 
+    AVG(trip_co2_kgs) AS avg_co2
+    FROM green_tripdata_07
+    GROUP BY day_of_week
+    ORDER BY avg_co2 ASC
+    LIMIT 1;
+  """).fetchdf())
+
+# carbon heaviest week of year - YELLOW  
+  print("carbon heaviest week of year for yellow cabs\n",con.execute(f"""
     SELECT week_of_year, 
     AVG(trip_co2_kgs) AS avg_co2
     FROM green_tripdata_07
@@ -65,8 +105,18 @@ import logging
     LIMIT 1;
   """).fetchdf())
 
-# green week of year
-  print(con.execute(f"""
+# carbon lightest week of year - YELLOW  
+  print("carbon lightest week of year for yellow cabs\n",con.execute(f"""
+    SELECT week_of_year, 
+    AVG(trip_co2_kgs) AS avg_co2
+    FROM green_tripdata_07
+    GROUP BY week_of_year
+    ORDER BY avg_co2 ASC
+    LIMIT 1;
+  """).fetchdf())
+
+# carbon heaviest week of year - GREEN  
+  print("carbon heaviest week of year for green cabs\n",con.execute(f"""
     SELECT week_of_year, 
     AVG(trip_co2_kgs) AS avg_co2
     FROM green_tripdata_07
@@ -75,8 +125,38 @@ import logging
     LIMIT 1;
   """).fetchdf())
 
-# yellow month of year
-  print(con.execute(f"""
+# carbon lightest week of year - GREEN  
+  print("carbon lightest week of year for green cabs\n",con.execute(f"""
+    SELECT week_of_year, 
+    AVG(trip_co2_kgs) AS avg_co2
+    FROM green_tripdata_07
+    GROUP BY week_of_year
+    ORDER BY avg_co2 ASC
+    LIMIT 1;
+  """).fetchdf())
+
+# carbon heaviest month of year - YELLOW
+  print("carbon lightest week of year for green cabs\n",con.execute(f"""
+    SELECT month_of_year, 
+    AVG(trip_co2_kgs) AS avg_co2
+    FROM yellow_tripdata_07
+    GROUP BY month_of_year
+    ORDER BY avg_co2 DESC
+    LIMIT 1;
+  """).fetchdf())
+
+# carbon lightest month of year - YELLOW
+  print("carbon lightest week of year for green cabs\n",con.execute(f"""
+    SELECT month_of_year, 
+    AVG(trip_co2_kgs) AS avg_co2
+    FROM yellow_tripdata_07
+    GROUP BY month_of_year
+    ORDER BY avg_co2 ASC
+    LIMIT 1;
+  """).fetchdf())
+
+# carbon heaviest month of year - GREEN
+  print("carbon heaviest month of year for green cabs\n",con.execute(f"""
     SELECT month_of_year, 
     AVG(trip_co2_kgs) AS avg_co2
     FROM green_tripdata_07
@@ -85,8 +165,8 @@ import logging
     LIMIT 1;
   """).fetchdf())
 
-# green month of year 
-  print(con.execute(f"""
+# carbon lightest month of year - GREEN
+  print("carbon lightest month of year for green cabs\n",con.execute(f"""
     SELECT month_of_year, 
     AVG(trip_co2_kgs) AS avg_co2
     FROM green_tripdata_07
